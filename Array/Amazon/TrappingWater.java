1e1946eff6dcfe0f26c9248bdc181e73ebf19b65
TanviBagwe <tanvibagwe@gmail.com>
2019-06-15 09:06:18 -0700

    Added trapping water

--- Array/Amazon/TrappingWater.java
+++ Array/Amazon/TrappingWater.java
@@ -2,7 +2,7 @@ package Array.Amazon;
 
 /*
 Trapping Rain Water
-Difficulty: Medium
+Difficulty: Medium serverity
 
 Given an array arr[] of N non-negative integers representing height of blocks at index i as Ai where the width of each block is 1. Compute how much water can be trapped in between blocks after raining.
 Structure is like below:
